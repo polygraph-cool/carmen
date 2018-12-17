ca9478415fde0ebf5ba559f529e3fa6b3db8384f
russellgoldenberg <russellgoldenberg@gmail.com>
2018-12-17 18:46:47 -0500

    dots

--- src/js/explore.js
+++ src/js/explore.js
@@ -1,18 +1,29 @@
 import $ from './dom';
+import Tweet from './tweet';
 
-const origW = 1280;
-const origH = 1024;
-let radius = 2.5;
+const ORIG_W = 1280;
+const ORIG_H = 1024;
+const DEFAULT_R = 2.5;
 
 const $explore = d3.select('#explore');
-const $dots = $explore.select('.explore__dots');
+const $dots = $explore.select('.figure__dots');
+const $tweets = $explore.select('.figure__tweets');
+
+const exampleTweet = {
+	name: 'The Pudding',
+	handle: '@puddingviz',
+	text: 'We ❤️ Carmen Sandiego',
+	time: '11/14/18 12:39 PM'
+};
 
 function handoff(direction) {}
+
+function showTweet() {}
 
 function resize() {
 	const width = $explore.node().offsetWidth;
 	const count = 130000;
-	radius = Math.ceil((radius * width) / origW);
+	const radius = Math.ceil((DEFAULT_R * width) / ORIG_W);
 	const col = Math.floor(count / width);
 	const row = Math.floor(count / col);
 	$dots.st({
@@ -23,6 +34,7 @@ function resize() {
 
 function init() {
 	resize();
+	$explore.select('button').on('click', showTweet);
 }
 
 export default { init, resize, handoff };
