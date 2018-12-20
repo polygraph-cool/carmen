0d999d5402a705bc6476257be0768b7bfbe63567
Amber Thomas <ProQuestionAsker@gmail.com>
2018-12-20 14:11:14 -0800

    Making explore section short and wide

--- src/js/explore.js
+++ src/js/explore.js
@@ -8,6 +8,7 @@ const DEFAULT_R = 2.5;
 const $explore = d3.select('#explore');
 const $dots = $explore.select('.figure__dots');
 const $tweets = $explore.select('.figure__tweets');
+const $figure = $explore.select('.explore__figure')
 
 const exampleTweet = {
 	name: 'The Pudding',
@@ -21,19 +22,27 @@ function handoff(direction) {}
 function showTweet() {}
 
 function resize() {
-	const width = $explore.node().offsetWidth;
+	const headerH = $.header.node().offsetHeight;
+	const height = window.innerHeight - headerH;
+	$figure.st('height', height)
+
 	const count = 130000;
-	const radius = Math.ceil((DEFAULT_R * width) / ORIG_W);
-	const col = Math.floor(count / width);
-	const row = Math.floor(count / col);
+	const radius = Math.ceil((DEFAULT_R * height) / ORIG_H);
+	const diameter = radius * 2
+	const row = Math.floor(count / height);
+	const col = Math.floor(count / row);
+	console.log({row, col, radius})
+
 	$dots.st({
-		height: row * radius,
+		height: height,
+		width: col * radius,
 		backgroundSize: `${radius * 2}px ${radius * 2}px`
 	});
 }
 
 function init() {
 	$explore.select('button').on('click', showTweet);
+	resize()
 }
 
 export default { init, resize, handoff };
