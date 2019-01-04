ffbbadcca40eb32a96bf33d36d7a8f48b4c90d5d
Amber Thomas <ProQuestionAsker@gmail.com>
2019-01-03 16:24:03 -0800

    Concentric circles to match mockup

--- src/js/intro.js
+++ src/js/intro.js
@@ -68,7 +68,8 @@ function triggerExample() {
 	Tweet.clear({ section: 'intro' });
 	Render.clear($.contextEx);
 	console.log({ d });
-	Render.dot({ d, ctx: $.contextEx, fill: '#fff' });
+	Render.dot({ d, ctx: $.contextEx, fill: '#fff', concentric: true });
+	//Render.concentric({ d, ctx: $.contextEx, stroke: '#fff' })
 	Tweet.create({
 		data,
 		x,
